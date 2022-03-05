3bdbd0d4f2b18d93a150bc7952e1fa38fb1b029f
Huong Pham <40278737+ht-pham@users.noreply.github.com>
2022-03-04 23:23:19 -0600

    The project with DateTimeWizard class is built
    The DateTimeWizard class is built with basic attributes: 
    1. String[] timeFormat = {"AM","PM","24HR"} for easy modification on time display & String timeDisplay is for the selected time format to be display with a default value as "24HR"; 
    2. Integer variables for date,month,year,hour,min;
    3. private Calendar datetime //This attribute is for future use of default Java functions in Calendar class.
    4. private TimeZone timezone = TimeZone.getTimeZone("CST"); //Similar to the previous attribute, it is for future usage & default timezone value is Central time
    The main class temporarily display the midterm check point date only.

---
+++ DateTimeWizard/README.md
@@ -0,0 +1,29 @@
+# CS5103 Course Project: Software Engineering Practice
+
+<div>
+  <b>Overview</b>
+  <br> 
+  This is a programming project for the graduate-level course CS5103 Software Engineering @UTSA. The project is written in Java language. 
+</div>
+
+<div>
+  <b>Goal of the project</b>
+  <br>
+  The goal of this project is to transform the given date time string to different formats.     
+</div>
+
+<!---
+  1. Requirement Engineering: Write user stories and test cases of the program you are writing.
+  2. Design: Adapt your software design based on new requirements posted later.
+  3. Implementation: Implement your code based on version control system and make changes to implementation based on new requirements.
+  4. Testing: Write unit tests for your classes.
+  5. Tool Application: Apply code clone detection, static bug detection on your code base and report results.
+--->
+
+<div>
+  <b>Requirements</b>
+  <br>
+  The initial requirement is to transform it to any different time zone.
+  <br>
+</div>
+
---
+++ DateTimeWizard/src/datetimewizard/DateTimeWizard.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package datetimewizard;
+import java.util.*;
+
+import java.time.*;
+import java.util.Scanner;
+/**
+ *
+ * @author huongpham
+ */
+public class DateTimeWizard {
+    private final String[] timeFormat = {"AM","PM","24HR"};
+    private String timeDisplay = timeFormat[2];
+    private int date,month,year,hour,min;
+    private Calendar datetime;
+    private TimeZone timezone = TimeZone.getTimeZone("CST");//default
+    //Stuct
+    public DateTimeWizard(){
+        //the current date at the default timezone
+        this.datetime = Calendar.getInstance(timezone);
+        
+    }
+    
+    public void setDate(int day, int month, int year){
+        this.datetime.set(year, month, day);
+        this.date = day;
+        this.month = month;
+        this.year = year;
+    }
+    
+    public void setTime(int hour,int minute,int formatID){
+        this.hour = hour;
+        this.min = minute;
+        if ((formatID<3)&&(formatID>=0)){
+            this.timeDisplay = timeFormat[formatID];
+        }
+    }
+    public String returnDate(){
+        String defaultDateFormat = this.month+"/"+this.date+"/"+this.year;
+        String defaultTimeFormat ;
+        if (this.timeDisplay.matches(timeFormat[2])){
+            defaultTimeFormat = this.hour+"H"+this.min+"M";
+        }else{    
+            defaultTimeFormat = this.hour+":"+this.min+" "+this.timeDisplay;
+        }
+            
+        return defaultDateFormat+" "+defaultTimeFormat;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DateTimeWizard today = new DateTimeWizard();
+        today.setDate(17,3,2022);
+        today.setTime(11,59,1);
+        System.out.println(today.returnDate());
+    }
+}
