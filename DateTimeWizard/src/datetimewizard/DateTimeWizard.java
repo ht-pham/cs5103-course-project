11ab4f76f491994fc61867900e3240016e8aa82d
Huong Pham <40278737+ht-pham@users.noreply.github.com>
2022-03-09 13:17:52 -0600

    Add setTimeZone function & Examples
    This function is to change the time zone to another area. It is built to acquire further requirement (i.e. change the local time to the concurrent time at other locations)

--- DateTimeWizard/src/datetimewizard/DateTimeWizard.java
+++ DateTimeWizard/src/datetimewizard/DateTimeWizard.java
@@ -38,6 +38,25 @@ public class DateTimeWizard {
             this.timeDisplay = timeFormat[formatID];
         }
     }
+    //This function is to change the current time zone to a new one
+    public void setTimeZone(TimeZone currentTimeZone,int change,boolean toEast){
+        String TimeZoneID = currentTimeZone.getID();
+        String zone = TimeZoneID.substring(4,6);
+        
+        int newZone = Integer.parseInt(zone);
+        
+        int newHour;
+        if(toEast==true){
+            newHour = newZone-change;
+        }else{
+            newHour = newZone+change;
+        }
+        String newZoneHour = String.valueOf(newHour);
+        TimeZoneID=TimeZoneID.replace(zone, newZoneHour);
+        
+        this.timezone = TimeZone.getTimeZone(TimeZoneID);
+    }
+    
     public String returnDate(){
         String defaultDateFormat = this.month+"/"+this.date+"/"+this.year;
         String defaultTimeFormat ;
@@ -53,17 +72,37 @@ public class DateTimeWizard {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        DateTimeWizard today = new DateTimeWizard();
-        today.setDate(17,3,2022);
-        today.setTime(11,59,1);
-        System.out.println(today.returnDate());
+        /** The following block of code is to show default examples of three different types of hour display
+        ** will come back to clean this block of code **
+        **/
+        DateTimeWizard dueDate = new DateTimeWizard();
+        dueDate.setDate(17,3,2022);
+        dueDate.setTime(11,59,1);
+        System.out.println("Midterm checkpoint date: "+dueDate.returnDate()
+                            +" "+dueDate.timezone.getDisplayName());
+        
+        //Convert the deadline as on Pacific Time Zone
+        dueDate.setTimeZone(dueDate.timezone,2,false);
+        System.out.println("New timezone: "+dueDate.timezone.getDisplayName());
+        dueDate.setTime(21,59,2);
+        System.out.println("Midterm checkpoint date: "+dueDate.returnDate()
+                            +" "+dueDate.timezone.getDisplayName());
+
+        //Convert the deadline as on Eastern Time Zone
+        dueDate.setTimeZone(dueDate.timezone,1,true);
+        System.out.println("New timezone: "+dueDate.timezone.getDisplayName());
+        dueDate.setDate(18,3,2022);
+        dueDate.setTime(0,59,0);
+        System.out.println("Midterm checkpoint date: "+dueDate.returnDate()
+                            +" "+dueDate.timezone.getDisplayName());
+        
         
         //This is for user input when the project is run in terminal
         //Command: java $PATH/DateTimeWizard.java [Date] [HourAM/PM/24H] [TimeZone]
         //Current expected result: User's orignal input 
         if (args.length>0){
             for (String s: args) {
-                System.out.print(s+"\t");
+                System.out.print(s+" ");
             }
         }
     }
